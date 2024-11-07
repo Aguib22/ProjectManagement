36587bad70dbbe40412319030f117d76c5a48839
Aguib22 <sowthiernaguibou53@gmail.ocm>
2024-11-07 12:58:29 +0000

    first commit

---
+++ src/test/java/com/ProjectManagement/digitalis/DigitalisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProjectManagement.digitalis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
